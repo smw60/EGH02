f04d86a6db147a20dd965ee031278e512dacc71a
BlinovaE <eugenia.blinova@gmail.com>
2017-12-12 08:51:31 +0300

    water by temp

--- EGH01/EGH01DB/Primitives/Helper1.cs
+++ EGH01/EGH01DB/Primitives/Helper1.cs
@@ -30,57 +30,8 @@ namespace EGH01DB.Primitives
             if (d > 1.0f) rc = (point1.height - point2.height) / d;     // угол    
             return rc;
         }
-        
 
 
-        
-        //static public bool GetListRiskObjectByLike(EGH01DB.IDBContext dbcontext, string findstring, ref List<RiskObject> listobj)
-        //{
-        //    bool rc = false;
-        //    using (SqlCommand cmd = new SqlCommand("EGH.GetRiskObjectListByLike", dbcontext.connection))
-        //    {
-        //        cmd.CommandType = CommandType.StoredProcedure;
-                
-        //        {
-        //            SqlParameter parm = new SqlParameter("@findstring", SqlDbType.VarChar);
-        //            parm.Value = findstring;
-        //            cmd.Parameters.Add(parm);
-        //        }
-        //        {
-        //            SqlParameter parm = new SqlParameter("@exitrc", SqlDbType.Int);
-        //            parm.Direction = ParameterDirection.ReturnValue;
-        //            cmd.Parameters.Add(parm);
-        //        }
-        //        try
-        //        {
-        //            SqlDataReader reader = cmd.ExecuteReader();
-        //            List<int>  listid = new List<int>();
-        //            while (reader.Read()) listid.Add((int)reader["IdТехногенногоОбъекта"]);
-        //            reader.Close();
-
-        //            if (rc = listid.Count > 0)
-        //            {  
-        //                RiskObject o = null;
-        //                foreach (int id in listid)
-        //                { 
-        //                   if (RiskObject.GetById(dbcontext,id, ref o))
-        //                   {
-        //                       listobj.Add(o);
-        //                   }
-        //                 }
-        //                rc = (listid.Count == listobj.Count());
-        //            }
-
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            rc = false;
-        //        };
-        //        return rc;
-
-        //    }
-
-        //}
      }
+}
     
- }
